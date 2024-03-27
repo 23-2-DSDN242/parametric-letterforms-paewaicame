63589226e61fdea59e69fe8e3252ddd44e31062f
Cameron Paewai <paewaicame@myvuw.ac.nz>
2024-03-28 12:18:49 +1300

    Initial Spacelander recreation

--- sketch.js
+++ sketch.js
@@ -13,74 +13,184 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
-}
+    arches: [
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: true,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: true,
+                orientation: "vertical"
+            }
+        },
+    ],
+    dot: true
+};
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
-}
+    arches: [
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: false,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+    ],
+    dot: true
+};
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+    arches: [
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: true,
+                orientation: "horizontal"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: true,
+                orientation: "horizontal"
+            }
+        },
+        {
+            visible: true,
+            gap: {
+                visible: false,
+                orientation: "vertical"
+            }
+        },
+    ],
+    dot: false
+};
+
+const backgroundColor = "#E99A08";
+
+function setup() {
+    // create the drawing canvas, save the canvas element
+    main_canvas = createCanvas(canvasWidth, canvasHeight);
+    main_canvas.parent('canvasContainer');
+
+    // color/stroke setup
+    strokeWeight(4);
+
+    // with no animation, redrawing the screen is not necessary
+    noLoop();
 }
 
-const backgroundColor  = "#acf2e7";
+function draw() {
+    // clear screen
+    background(backgroundColor);
 
-const darkGreen  = "#26b29d";
-const lightGreen  = "#30dfc4";
-const strokeColor  = "#0a2d27";
+    // compute the center of the canvas
+    let center_x = canvasWidth / 2;
+    let center_y = canvasHeight / 2;
 
-function setup () {
-  // create the drawing canvas, save the canvas element
-  main_canvas = createCanvas(canvasWidth, canvasHeight);
-  main_canvas.parent('canvasContainer');
+    // draw the letters A, B, C from saved data
+    drawLetter(center_x - 250, center_y, letterA);
+    drawLetter(center_x, center_y, letterB);
+    drawLetter(center_x + 250, center_y, letterC);
 
-  // color/stroke setup
-  stroke(strokeColor);
-  strokeWeight(4);
-
-  // with no animation, redrawing the screen is not necessary
-  noLoop();
 }
 
-function draw () {
-  // clear screen
-  background(backgroundColor);
-
-  // compute the center of the canvas
-  let center_x = canvasWidth / 2;
-  let center_y = canvasHeight / 1.6;
-
-  // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, letterA);
-  drawLetter(center_x      , center_y, letterB);
-  drawLetter(center_x + 250, center_y, letterC);
+let imgDot;
+let imgArch;
+let imgArchGapVertical;
+let imgArchGapHorizontal;
+function preload() {
+    imgDot = loadImage('components/dot.png');
+    imgArch = loadImage('components/arch.png');
+    imgArchGapVertical = loadImage('components/archgapvertical.png');
+    imgArchGapHorizontal = loadImage('components/archgaphorizontal.png');
 }
 
-function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+function drawLetter(x, y, letterData) {
+    let imageSize = 100;
 
-  // draw two circles
-  fill(darkGreen);
-  ellipse(posx, posy, 150, 150);
-  fill(lightGreen);
-  ellipse(pos2x, pos2y, size2, size2);
+    let scaleValues = [
+        [1, 1],
+        [-1, 1],
+        [-1, -1],
+        [1, -1],
+    ];
+    for (let i = 0; i < 4; i++) {
+        if (letterData.arches[i].visible) {
+            push();
+            translate(x, y);
+            scale(scaleValues[i]);
+            if (letterData.arches[i].gap.visible) {
+                if (letterData.arches[i].gap.orientation == "vertical") {
+                    image(imgArchGapVertical, -imageSize / 2, -imageSize / 2);
+                } else if (letterData.arches[i].gap.orientation == "horizontal") {
+                    image(imgArchGapHorizontal, -imageSize / 2, -imageSize / 2);
+                }
+            } else {
+                image(imgArch, -imageSize / 2, -imageSize / 2);
+            }
+            pop();
+        }
+    }
+
+    if (letterData.dot) {
+        image(imgDot, x - imageSize / 2, y - imageSize / 2);
+    }
 }
 
 function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
-  else if (key == '@') {
-    saveBlocksImages(true);
-  }
+    if (key == '!') {
+        saveBlocksImages();
+    }
+    else if (key == '@') {
+        saveBlocksImages(true);
+    }
 }
